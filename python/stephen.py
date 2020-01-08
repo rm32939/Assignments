ffe0798d9432e7e2fba18914c07918d0761f71d3
rm32939 <59250159+rm32939@users.noreply.github.com>
2020-01-08 17:25:10 +0530

    Add files via upload

---
+++ python/stephen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def green():
+    print("High?")
+
+
+# In[2]:
+
+
+print(__name__)
+
+
+# In[3]:
+
+
+if __name__ == "__main__":
+    def burgundy():
+        print("Interesting color")
+        
+    
+
+
+# In[ ]:
+
+
+
+
